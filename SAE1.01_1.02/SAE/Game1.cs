8de05d716a15bb205a764bb68fc5999fe0a963fd
Maureen Bozon-Leydier <maureen.bozon-leydier@etu.univ-smb.fr>
2023-01-04 11:33:54 +0100

    Page d'accueil

--- SAE1.01_1.02/SAE/Game1.cs
+++ SAE1.01_1.02/SAE/Game1.cs
@@ -44,11 +44,15 @@ namespace SAE
 
 
         //TAILLE FENETRE
-        public const int TAILLE_FENETRE = 800;
+        public const int TAILLE_FENETRE_L = 950;
+        public const int TAILLE_FENETRE_H = 700;
 
 
 
         //ECRAN ACCUEIL
+        //fond ecran
+        private Texture2D _textureFond;
+        private Vector2 _positionFond;
         //jouer
         private string _jouer;
         private SpriteFont _policeJouer;
@@ -79,23 +83,24 @@ namespace SAE
             Window.Title = "Haunted Manor"; //titre de la fenêtre
 
             //FENETRE
-            _graphics.PreferredBackBufferWidth = TAILLE_FENETRE;
-            _graphics.PreferredBackBufferHeight = TAILLE_FENETRE;
+            _graphics.PreferredBackBufferWidth = TAILLE_FENETRE_L;
+            _graphics.PreferredBackBufferHeight = TAILLE_FENETRE_H;
             _graphics.ApplyChanges();
 
             //ACCUEIL
+            _positionFond = new Vector2(700, 900);
             //jouer
             _jouer = "JOUER";
             _policeJouer = Content.Load<SpriteFont>("Font");
-            _positionJouer = new Vector2(300, 300);
+            _positionJouer = new Vector2(400, 300);
             //régle
-            _regle = "Règles du jeu";
+            _regle = "Regles du jeu";
             _policeRegle = Content.Load<SpriteFont>("Font");
-            _positionRegle = new Vector2(400, 400);
+            _positionRegle = new Vector2(600, 550);
             //commande
             _commande = "Commandes";
             _policeCommande = Content.Load<SpriteFont>("Font");
-            _positionCommande = new Vector2(100, 400);
+            _positionCommande = new Vector2(150, 550); 
 
             base.Initialize();
             //beredsferd
@@ -104,10 +109,11 @@ namespace SAE
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _textureFond = Content.Load<Texture2D>("accueil");
 
             //GEORGE            
-           /* SpriteSheet spriteSheet = Content.Load<SpriteSheet>("george.sf", new JsonContentLoader()); //NE MARCHE PAS
-            _perso = new AnimatedSprite(spriteSheet);*/
+            /*     SpriteSheet spriteSheet = Content.Load<SpriteSheet>("george.sf", new JsonContentLoader()); //NE MARCHE PAS
+                _perso = new AnimatedSprite(spriteSheet);*/
 
             // TODO: use this.Content to load your game content here
         }
@@ -119,7 +125,7 @@ namespace SAE
 
             // TODO: Add your update logic here
             //GEORGE
-            _perso.Play("gBas"); // une des animations définies dans « animation.sf »
+          //  _perso.Play("gBas"); // une des animations définies dans « george.sf »
             
             base.Update(gameTime);
         }
@@ -129,16 +135,16 @@ namespace SAE
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            GraphicsDevice.Clear(Color.Red);
-
+            _spriteBatch.Begin();
             //ACCUEIL
+            _spriteBatch.Draw(_textureFond, new Rectangle(0, 0, 1000, 700), Color.White);
             _spriteBatch.DrawString(_policeJouer, $"{_jouer}", _positionJouer, Color.White);
             _spriteBatch.DrawString(_policeRegle, $"{_regle}", _positionRegle, Color.White);
-            _spriteBatch.DrawString(_policeCommande, $"{_commande}", _positionCommande, Color.White);
+            _spriteBatch.DrawString(_policeCommande, $"{_commande}", _positionCommande, Color.White); 
 
             //GEORGE
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(_perso, _positionPerso);
+
+            //     _spriteBatch.Draw(_perso, _positionPerso);
             _spriteBatch.End();
 
             
